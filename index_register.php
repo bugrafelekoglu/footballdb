47107f00b37fe83141bab4a2b04994499d935520
barispoyraz <barispoyraz@users.noreply.github.com>
2017-05-20 19:40:45 +0300

    Add files via upload

--- index_register.php
+++ index_register.php
@@ -35,44 +35,6 @@
             header("location: index_register.php");
         }
     }
-
-    //REGISTER
-    if(isset($_SESSION['regUser'])){
-
-        //FULLNAME
-
-        $fullName = trim($_POST['full_name']);
-        //$fullName = strip_tags($fullName);
-        //$fullName = htmlspecialchars($fullName);
-
-        //USERNAME
-
-        $username = trim($_POST['user_name']);
-        //$username = strip_tags($fullName);
-        //$username = htmlspecialchars($fullName);
-
-        //EMAIL
-
-        $email = trim($_POST['email']);
-        //$email = strip_tags($email);
-        //$email = htmlspecialchars($email);
-
-        //PASSWORD
-
-        $password = trim($_POST['password']);
-        //$password = strip_tags($password);
-        //$password = htmlspecialchars($password);
-
-        //CONFIRMPASSWORD
-
-        $confirmPassword = trim($_POST['password_confirmation']);
-        //$confirmPassword = strip_tags($confirmPassword);
-        //$confirmPassword = htmlspecialchars($confirmPassword);
-
-        //TODO: VALIDATION
-        $query = "INSERT INTO Person(username, password, email, fullname, nationality)
-                    VALUES ('$username', '$password', '$email', '$fullName', '')";
-    }
 ?>
 
 <!DOCTYPE html>
