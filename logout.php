e9831aaf023f04245c826b2f325fce8976677d59
Buğra Felekoğlu <bugrafelekoglu@gmail.com>
2017-05-20 19:50:43 +0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -2,7 +2,7 @@
     session_start();
     session_unset();
     if(session_destroy()){
-        header("location: index_register.php");
+        header("location: index.php");
     }
     exit();
 ?>
